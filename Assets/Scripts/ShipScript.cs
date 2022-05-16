b72ff4ad4d9eeb8889aecc459d4e411448a78fd7
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-16 12:28:16 +0530

    Asteroids prefabs were done.Asteroids are spawning.Object pool concept is used

--- Assets/Scripts/ShipScript.cs
+++ Assets/Scripts/ShipScript.cs
@@ -54,22 +54,7 @@ public class ShipScript : MonoBehaviour
         StartCoroutine(TURN_COROUTINE_FUNCTION, worldTouchPosition);
     }
 
-    /* IEnumerator TurnRotateMoveTowardsTap(Vector3 tempPoint)
-     {
-         isRotating=true;
-         //tempPoint=tempPoint-this.transform.position;   //To find the difference between touch position and current position
-        // tempPoint.z=transform.position.z;                //assigning ship position to touch position
-         Quaternion startRotation=this.transform.rotation;                              //The rotation start point
-         Quaternion endRotation=Quaternion.LookRotation(tempPoint,Vector3.up);         //This rotation will look at touchpoint in up direction 
-         float time=Quaternion.Angle(startRotation,endRotation)/rotationSpeed;                  //Angle between two rotations 
-         for(float i=0;i<time; i=i+Time.deltaTime)
-         {
-            transform.rotation= Quaternion.Slerp(startRotation,endRotation,i);
-         }
-         transform.rotation = endRotation; //we need to put shoot functionality here
-         isRotating = false;
-         yield return null;
-     }*/
+    
 
     IEnumerator TurnRotateOnTap(Vector3 tempPoint)
     {
